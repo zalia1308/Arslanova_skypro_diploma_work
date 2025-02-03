b643ec913cfc5dcc2e89285503f507fc27e63bff
zalia1308 <arslanovazalia2016@gmail.com>
2025-02-04 00:09:24 +0300

    Revert "Diploma_work"

--- config.py
+++
@@ -1,9 +0,0 @@
-basic_url_1 = 'https://kinopoiskapiunofficial.tech/api/v1/staff'
-basic_url_2 = 'https://kinopoiskapiunofficial.tech/api/v2.1/films'
-basic_url_3 = 'https://kinopoiskapiunofficial.tech/api/v2.2/films'
-token = 'd9a32e8d-c8e4-44df-b462-691441024b65'
-
-
-url = 'https://www.kinopoisk.ru/'
-login = 'ff'
-password = 'ff'
